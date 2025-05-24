38a6f06904614b0ec33eb16050afd5237c71fec7
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2025-05-24 19:39:25 +0600

    navbar, hero section,  Dropdown select with Filter

---
+++ assets/js/main.js
@@ -0,0 +1,372 @@
+"use strict";
+// toggleSideMenu start
+const toggleSideMenu = () => {
+  document.body.classList.toggle("toggle-sidebar");
+};
+// toggleSideMenu end
+
+// add bg to nav
+if ($("nav").length) {
+  const header = document.querySelector("nav");
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("active", window.scrollY >= 100);
+  });
+}
+
+$(document).ready(function () {
+  // Hero slider start
+  if ($(".hero-swiper").length) {
+    var swiper = new Swiper(".hero-swiper", {
+      preloadImages: false,
+      loop: true,
+      centeredSlides: false,
+      resistance: true,
+      resistanceRatio: 0.6,
+      speed: 2400,
+      spaceBetween: 0,
+      parallax: false,
+      effect: "fade",
+      // autoplay: false,
+      autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+
+  // cmn select2 start
+  $(".cmn-select2").select2();
+  // cmn select2 end
+
+  // cmn-select2 with image start
+  $(".cmn-select2-image").select2({
+    templateResult: formatState,
+    templateSelection: formatState,
+  });
+  function formatState(state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var baseUrl = "assets/img/mini-flag";
+    var $state = $(
+      '<span><img src="' +
+        baseUrl +
+        "/" +
+        state.element.value.toLowerCase() +
+        '.svg" class="img-flag" /> ' +
+        state.text +
+        "</span>"
+    );
+    return $state;
+  }
+  function formatState(state) {
+    if (!state.id) {
+      return state.text;
+    }
+
+    var baseUrl = "assets/img/mini-flag";
+    var $state = $('<span><img class="img-flag" /> <span></span></span>');
+
+    $state.find("span").text(state.text);
+    $state
+      .find("img")
+      .attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".svg");
+
+    return $state;
+  }
+  // cmn-select2 with image end
+
+  // Modal select2 with image start
+  $(".modal-select2-image").select2({
+    dropdownParent: $("#formModal"),
+    templateResult: formatState,
+    templateSelection: formatState,
+  });
+  function formatState(state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var baseUrl = "assets/img/mini-flag";
+    var $state = $(
+      '<span><img src="' +
+        baseUrl +
+        "/" +
+        state.element.value.toLowerCase() +
+        '.svg" class="img-flag" /> ' +
+        state.text +
+        "</span>"
+    );
+    return $state;
+  }
+  function formatState(state) {
+    if (!state.id) {
+      return state.text;
+    }
+
+    var baseUrl = "assets/img/mini-flag";
+    var $state = $('<span><img class="img-flag" /> <span></span></span>');
+
+    $state.find("span").text(state.text);
+    $state
+      .find("img")
+      .attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".svg");
+
+    return $state;
+  }
+  // MOdal select2 with image end
+
+  // Cmn select2 tags start
+  $(".cmn-select2-tags").select2({
+    tags: true,
+  });
+  // Cmn select2 tags end
+
+  // cmn select2 modal start
+  $(".modal-select2").select2({
+    dropdownParent: $("#formModal"),
+  });
+  // cmn select2 modal start
+  // Range area start
+  if ($(".js-range-slider").length) {
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 100,
+      from: 800,
+      to: 500,
+      grid: true,
+    });
+  }
+  // Range area end
+  if ($("#myID").length) {
+    flatpickr("#myID", {
+      inline: true,
+      dateFormat: "d-m-Y",
+    });
+  }
+  // Round button animation start
+  $(function () {
+    $(".round-btn")
+      .on("mouseenter", function (e) {
+        var parentOffset = $(this).offset(),
+          relX = e.pageX - parentOffset.left,
+          relY = e.pageY - parentOffset.top;
+        $(this).find("span").css({ top: relY, left: relX });
+      })
+      .on("mouseout", function (e) {
+        var parentOffset = $(this).offset(),
+          relX = e.pageX - parentOffset.left,
+          relY = e.pageY - parentOffset.top;
+        $(this).find("span").css({ top: relY, left: relX });
+      });
+  });
+  // Round button animation end
+  AOS.init();
+});
+// Fancybox carousel section start
+if ($(".fancybox-carousel-section").length) {
+  // Initialise Carousel
+  const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+    Dots: false,
+  });
+
+  // Thumbnails
+  const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
+    Sync: {
+      target: mainCarousel,
+      friction: 0,
+    },
+    Dots: false,
+    Navigation: false,
+    center: true,
+    slidesPerPage: 1,
+    infinite: true,
+  });
+
+  // Customize Fancybox
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    Carousel: {
+      on: {
+        change: (that) => {
+          mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
+            friction: 0,
+          });
+        },
+      },
+    },
+  });
+}
+// Fancybox carousel section end
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+// Counter start
+if ($(".statistics-counter").length) {
+  $(".statistics-counter").counterUp({
+    delay: 10,
+    time: 2000,
+  });
+}
+// Copy text start
+function copyTextFunc() {
+  const element = document.querySelector(".docs-code");
+  const storage = document.createElement("textarea");
+  storage.value = element.innerHTML;
+  element.appendChild(storage);
+  storage.select();
+  storage.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  element.removeChild(storage);
+}
+// Copy text end
+
+// Social share start
+if ($("#shareBlock").length) {
+  $("#shareBlock").socialSharingPlugin({
+    urlShare: window.location.href,
+    description: $("meta[name=description]").attr("content"),
+    title: $("title").text(),
+  });
+}
+// Social share end
+
+// International Telephone Input start
+if ($("#telephone").length) {
+  const input = document.querySelector("#telephone");
+  window.intlTelInput(input, {
+    initialCountry: "bd",
+    separateDialCode: true,
+  });
+}
+// International Telephone Input end
+
+// Copy page url start
+if ($("#copyBtn").length) {
+  document.getElementById("copyBtn").addEventListener("click", () => {
+    let referralURL = document.getElementById("referralURL");
+    referralURL.select();
+    navigator.clipboard.writeText(referralURL.value);
+    if (referralURL.value) {
+      document.getElementById("copyBtn").innerHTML =
+        '<i class="fa-regular fa-circle-check"></i> Copied';
+      setTimeout(() => {
+        document.getElementById("copyBtn").innerHTML =
+          '<i class="fa-regular fa-copy"></i>copy';
+      }, 1000);
+    }
+  });
+}
+
+// Copy page url end
+
+// input field show hide password start
+if (document.querySelector(".login-register-form")) {
+  const passwordBoxes = document.querySelectorAll(".password-box");
+  passwordBoxes.forEach((passwordBox) => {
+    const passwordInput = passwordBox.querySelector(".password");
+    const passwordIcon = passwordBox.querySelector(".password-icon");
+
+    passwordIcon.addEventListener("click", function () {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        passwordIcon.classList.add("fa-eye-slash");
+        passwordIcon.classList.remove("fa-eye");
+      } else {
+        passwordInput.type = "password";
+        passwordIcon.classList.add("fa-eye");
+        passwordIcon.classList.remove("fa-eye-slash");
+      }
+    });
+  });
+}
+// input field show hide password end
+
+// Custom dropdown start
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleDropdownAttributes() {
+    const dropdowns = document.querySelectorAll(".custom-dropdown > a"); // Select all dropdown links
+
+    dropdowns.forEach((dropdown) => {
+      if (window.innerWidth >= 992) {
+        dropdown.removeAttribute("role");
+        dropdown.removeAttribute("data-bs-toggle");
+        dropdown.removeAttribute("aria-expanded");
+      } else {
+        dropdown.setAttribute("role", "button");
+        dropdown.setAttribute("data-bs-toggle", "dropdown");
+        dropdown.setAttribute("aria-expanded", "false");
+      }
+    });
+  }
+
+  // Run on page load
+  toggleDropdownAttributes();
+
+  // Run on window resize
+  window.addEventListener("resize", toggleDropdownAttributes);
+});
+// Custom dropdown end
+
+// Dropdown select with Filter end
+if ($(".search-box2").length) {
+  function handleSelect(searchBox2, searchInput, searchItem) {
+    searchInput.addEventListener("click", function (event) {
+      searchBox2.classList.add("active");
+      event.stopPropagation();
+    });
+
+    window.addEventListener("click", function () {
+      searchBox2.classList.remove("active");
+    });
+
+    searchItem.forEach(function (searchItemSingle) {
+      searchItemSingle.addEventListener("click", function () {
+        const text = searchItemSingle.querySelector(".title");
+        const textContent = text.textContent;
+        searchInput.value = textContent;
+        searchBox2.classList.remove("active");
+      });
+    });
+  }
+
+  const searchBox2 = document.querySelector("#search-box2");
+  const searchInput = document.querySelector("#search-input");
+  const searchItem = document.querySelectorAll("#search-result .search-item");
+  handleSelect(searchBox2, searchInput, searchItem);
+
+  // filter start
+  function filterItems(inputId, items) {
+    const input = document.getElementById(inputId);
+    const filter = input.value.toUpperCase();
+
+    items.forEach((item) => {
+      const title = item.querySelector(".title");
+      const txtValue = title.textContent || title.innerText;
+
+      if (txtValue.toUpperCase().includes(filter)) {
+        item.style.display = "";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  }
+  const filterSearchInputId = "search-input";
+  const filterSearchInput = document.getElementById(filterSearchInputId);
+  const items = document.querySelectorAll("#search-result .search-item");
+  filterSearchInput.addEventListener("keyup", function () {
+    filterItems(filterSearchInputId, items);
+  });
+}
+// Dropdown select with Filter end
